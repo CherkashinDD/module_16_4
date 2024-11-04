25f84660b0bb37d79c1058ab2eb1d1d984ec43ac
CherkashinDD <danl-0284@mail.ru>
2024-11-04 11:38:16 +1000

    Add files via upload

--- module_16_4.py
+++ module_16_4.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Body, HTTPException
-from typing import List
+from fastapi import FastAPI, Body, HTTPException, Path
+from typing import List, Annotated
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -19,27 +19,30 @@ async def get_users() -> List[User]:
 
 
 @app.post("/user/{username}/{age}")
-async def post_user(message: User) -> str:
-    message.id = len(users) + 1
-    users.append(message)
-    return f"id: {message.id}, username: {message.username}, age: {message.age}"
+async def post_user(
+        username: Annotated[str, Path(min_length=4, max_length=20, description='Enter username', example="Alex")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="40")]
+) -> str:
+    new_id = (users[-1].id + 1) if users else 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return f"id: {new_user.id}, username: {new_user.username}, age: {new_user.age}"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, usname: str = Body(), ag: int = Body()) -> str:
-    try:
-        edit_message = users[user_id - 1]
-        edit_message.username = usname
-        edit_message.age = ag
-        return f"id: {edit_message.id}, username: {edit_message.username}, age: {edit_message.age}"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+async def update_user(user_id: int, new_username: str = Body(), new_age: int = Body()) -> str:
+    for user in users:
+        if user.id == user_id:
+            user.username = new_username
+            user.age = new_age
+            return f"id: {user.id}, username: {user.username}, age: {user.age}"
+    raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int) -> str:
-    try:
-        user_del = users.pop(user_id - 1)
-        return f"id: {user_del.id}, username: {user_del.username}, age: {user_del.age}"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            user_del = users.pop(index)
+            return f"id: {user_del.id}, username: {user_del.username}, age: {user_del.age}"
+    raise HTTPException(status_code=404, detail="User was not found")
